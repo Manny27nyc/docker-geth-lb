cbdcd47b0d3bccadae7de1f37512828bdd049a0f
Andre Cronje <andre@mfin.io>
2018-04-28 09:09:25 +0200

    Wanchain added as deployment for MEW
    Signed-off-by: Andre Cronje <andre@mfin.io>

---
+++ wanchain-instances/files/startscript.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd /root/eth-net-intelligence-api/
+/usr/bin/pm2 start app.json
+while true
+do
+if [ ! `pgrep geth` ] ; then
+	gwan --fast --cache=512 --rpcvhosts='*' --rpc --rpcaddr '0.0.0.0' --rpccorsdomain '*' --txpool.accountslots=256 --txpool.globalslots=20000 --txpool.accountqueue=256 --txpool.globalqueue=20000
+fi
+sleep 10
+done
